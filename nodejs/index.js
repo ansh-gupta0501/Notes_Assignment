6cd3651289f1c0a917906870438dfef33f182d60
ansh-gupta0501 <ansh.gupta@innovasolutions.com>
2025-07-21 17:49:45 +0530

    node js

---
+++ nodejs/index.js
@@ -0,0 +1,19 @@
+// Module Wrapper
+
+// 5 parameters when module code converted to ife
+(function(exports,require,module,__filename,__dirname){
+    const superHero = 'Batman';
+    console.log(superHero)
+})
+
+
+// ----------------------------------
+
+
+
+
+
+
+
+
+
