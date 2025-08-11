ca7939db5e12c3b099b8a00376f2bf0768534aa3
ansh-gupta0501 <ansh.gupta@innovasolutions.com>
2025-08-11 17:59:46 +0530

    aws lambda layers

---
+++ awsLambda/layers2/nodejs/reusablefunction.mjs
@@ -0,0 +1,11 @@
+
+export function sayHello(){
+
+    console.log('Hello welcome to reusablefunction')
+}
+
+export function getDataFromDatabase(name){
+    // database command 
+    return {'name' : name}
+}
+
